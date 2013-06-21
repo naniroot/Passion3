e381cdeae11db006593856abb4c5d3a2a44babc4
naniroot <narayanark17@gmail.com>
2013-06-20 20:01:57 -0400

    just before genric views on 6th video

--- passion3/passion3/settings.py
+++ passion3/passion3/settings.py
@@ -129,6 +129,7 @@ INSTALLED_APPS = (
 
     'south',
     'taggit',
+    'annoying',
 
     'rants',
 )
--- passion3/rants/views.py
+++ passion3/rants/views.py
@@ -1,30 +1,29 @@
 import datetime
 
-from django.http import HttpResponse
-from django.shortcuts import render_to_response, get_object_or_404
-from django.template import RequestContext
+from django.shortcuts import get_object_or_404
+
+from annoying.decorators import render_to
 
 from rants.models import Rant, Post
 
+@render_to('index.html')
 def blog_index(request):
 	rants = Rant.objects.filter(active=True)
-	return render_to_response('index.html', {
-		'rants': rants,
-	}, context_instance=RequestContext(request)) 
+	
+	return { 'rants':rants }
 
+@render_to('rants/index.html')
 def rant(request, slug):
 	rant = get_object_or_404(Rant, active=True, slug=slug)
 
-	return render_to_response('rants/index.html', {
-		'rant': rant
-	}, context_instance=RequestContext(request))
+	return { 'rant': rant }
 
+@render_to('rants/post.html')
 def post(request, rant, slug):
 	post = get_object_or_404(Post, active=True, 
 				publish_at__lte=datetime.datetime.now(),
 				rant__slug=rant,
 				slug=slug)
-	return render_to_response('rants/post.html', {
-		'post': post,
-	}, context_instance=RequestContext(request))
+
+	return { 'post': post }
 	
